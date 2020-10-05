bd78dd59c2d9f990715b64ccf7362b0fb0e3b965
rokeby <gary.zhexi.zhang@gmail.com>
2020-10-05 18:37:57 +0100

    switch to mapboxGL added pitch bearing

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,24 +1,22 @@
 // hellloAPI key for Mapbox. Get one here: https://www.mapbox.com/studio/account/tokens/
 const key = 'pk.eyJ1Ijoicm9rZWJ5IiwiYSI6ImNrZWViMjYzdDBqcjUzMm1sZ2IzdmxvdXMifQ.ZQFCehPe0Z7IRpR3k6vlKQ';
-//
-
-
 
 // Create an instance of Mapbox
-const mappa = new Mappa('Mapbox', key);
+const mappa = new Mappa('MapboxGL', key);
 let myMap;
 
 let canvas;
 let hurricanes;
 
 function preload() {
-	  // Load the data
+	// Load the csv data
   hurricanes = loadTable('../data/katrina_latLng_cleaned.csv', 'csv', 'header');
 }
 
 function setup() {
   canvas = createCanvas(1200, 700);
 
+  // establish lat and long for map options (starting position)
   const startlatitude = Number(hurricanes.getString(1, 'lat'));
   const startlongitude = Number(hurricanes.getString(1, 'long'));
   print("first hurricane point is " + startlatitude, + ", " + startlongitude)
@@ -27,11 +25,13 @@ function setup() {
   const options = {
     lat: startlatitude,
     lng: startlongitude,
-    zoom: 6,
+    zoom: 10,
     studio: true, // false to use non studio styles
+    style: 'mapbox://styles/rokeby/ckfvgjjsy6vkw19mkij8g9v3c',
+    pitch: 45,
+    bearing: -30,
     //style: 'mapbox.dark' //streets, outdoors, light, dark, satellite (for nonstudio)
-    // style: 'mapbox://styles/rokeby/ckfvgjjsy6vkw19mkij8g9v3c',
-    style: 'mapbox://styles/mapbox/satellite-v9',
+    // style: 'mapbox://styles/mapbox/satellite-v9',
   };
 
   // Create a tile map and overlay the canvas on top.
@@ -40,7 +40,7 @@ function setup() {
   // Only redraw the meteorites when the map change and not every frame.
   myMap.onChange(drawHurricaneDots);
 
-  fill('#FF0000');
+  fill('rgba(255,0,0, 0.5)');
   stroke(10);
 
   print(hurricanes.getRowCount() + " total rows in table");
@@ -60,28 +60,22 @@ function drawHurricaneDots() {
     const latitude = Number(hurricanes.getString(i, 'lat'));
     const longitude = Number(hurricanes.getString(i, 'long'));
 
-    // Only draw them if the position is inside the current map bounds. We use a
-    // Mapbox method to check if the lat and lng are contain inside the current
-    // map. This way we draw just what we are going to see and not everything. See
-    // getBounds() in https://www.mapbox.com/mapbox.js/api/v3.1.1/l-latlngbounds/
       // Transform lat/lng to pixel position
       const latlong = myMap.latLngToPixel(latitude, longitude);
+      
+      // append pixel positions to an array pos
       append(pos, latlong)
-      // Get the size of the meteorite and map it. 60000000 is the mass of the largest
-      // meteorite (https://en.wikipedia.org/wiki/Hoba_meteorite)
-      //ellipse(latlong.x, latlong.y, 10, 10);
-      if(i > 0) {
-          line(pos[i].x, pos[i].y, pos[i-1].x, pos[i-1].y)
-          strokeWeight(50)
-          stroke('#F0F')
-      }
+      
+       ellipse(latlong.x, latlong.y, 50, 50);
+      //rect((latlong.x), (latlong.y), (latlong.x + 5), (latlong.y + 5),)
+     
+      // if(i > 0) {
+      //     line(pos[i].x, pos[i].y, pos[i-1].x, pos[i-1].y)
+      //     strokeWeight(20)
+      //     stroke('#F0F')
+      //     smooth()
+      // }
   }
-
-  //   for (let i = 0; i < pos.length; i += 1) {
-  //   line(pos[i].x, pos[i].y, pos[i+1].x, pos[i+1].y)            
-  // }
-
-  
 
    print(pos)    
 
