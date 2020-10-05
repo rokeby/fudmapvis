a0bd533d51d021f5f67d7712904e8101beec94ca
rokeby <gary.zhexi.zhang@gmail.com>
2020-10-05 01:00:54 +0100

    made a line

--- p5/sketch.js
+++ p5/sketch.js
@@ -27,10 +27,11 @@ function setup() {
   const options = {
     lat: startlatitude,
     lng: startlongitude,
-    zoom: 8,
+    zoom: 6,
     studio: true, // false to use non studio styles
     //style: 'mapbox.dark' //streets, outdoors, light, dark, satellite (for nonstudio)
-    style: 'mapbox://styles/rokeby/ckfvgjjsy6vkw19mkij8g9v3c',
+    // style: 'mapbox://styles/rokeby/ckfvgjjsy6vkw19mkij8g9v3c',
+    style: 'mapbox://styles/mapbox/satellite-v9',
   };
 
   // Create a tile map and overlay the canvas on top.
@@ -51,6 +52,8 @@ function draw() {}
 function drawHurricaneDots() {
   // Clear the canvas
   clear();
+  
+  let pos = []
 
   for (let i = 0; i < hurricanes.getRowCount(); i += 1) {
     // Get the lat/lng of each meteorite
@@ -62,10 +65,24 @@ function drawHurricaneDots() {
     // map. This way we draw just what we are going to see and not everything. See
     // getBounds() in https://www.mapbox.com/mapbox.js/api/v3.1.1/l-latlngbounds/
       // Transform lat/lng to pixel position
-      const pos = myMap.latLngToPixel(latitude, longitude);
+      const latlong = myMap.latLngToPixel(latitude, longitude);
+      append(pos, latlong)
       // Get the size of the meteorite and map it. 60000000 is the mass of the largest
       // meteorite (https://en.wikipedia.org/wiki/Hoba_meteorite)
-      ellipse(pos.x, pos.y, 10, 10);
-    
+      //ellipse(latlong.x, latlong.y, 10, 10);
+      if(i > 0) {
+          line(pos[i].x, pos[i].y, pos[i-1].x, pos[i-1].y)
+          strokeWeight(50)
+          stroke('#F0F')
+      }
   }
+
+  //   for (let i = 0; i < pos.length; i += 1) {
+  //   line(pos[i].x, pos[i].y, pos[i+1].x, pos[i+1].y)            
+  // }
+
+  
+
+   print(pos)    
+
 }
