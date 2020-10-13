20df311a4fde8423ac0609883458fdb316d88421
rokeby <gary.zhexi.zhang@gmail.com>
2020-10-13 14:06:54 +0100

    added backup vis

--- p5/fudmapvis.js
+++ p5/fudmapvis.js
@@ -6,24 +6,32 @@ let canvas;
 let hurricanes;
 const geoJSONlatlong = []
 const zoom = 7
+let preJSON
+
+function preload() {
+
+  preJSON = loadJSON('http://server.fud.global/hurricane');
+
+}
 
 function setup() {
 
   var canvas = createCanvas(windowWidth, windowHeight);
-  canvas.style('display', 'block')
+  canvas.style('display', 'block');
   canvas.parent('mapvis');
 
+  console.log("prejson is", preJSON)
+  console.log("initial coords", preJSON[0].geometry.coordinates)
+  console.log("prejson length", preJSON.length)
+
   const options = {
-    lat: 0,
-    lng: 0,
+    lat: preJSON[0].geometry.coordinates[1],
+    lng: preJSON[0].geometry.coordinates[0],
     zoom: zoom,
     studio: true, // false to use non studio styles
     style: 'mapbox://styles/rokeby/ckfvgjjsy6vkw19mkij8g9v3c',
     pitch: 50,
     bearing: 0,
-    // style: 'mapbox://styles/rokeby/ckfzdwcyi058w19s0w40c1i25',
-    // style: 'mapbox.dark' //streets, outdoors, light, dark, satellite (for nonstudio)
-    // style: 'mapbox://styles/mapbox/satellite-v9',
   };
 
   myMap = mappa.tileMap(options);
@@ -48,23 +56,12 @@ function initiateHurricane () {
           if (err) throw err;
 
           var coordinates = []
-          // myMap.map.addSource('trace', { type: 'geojson', data: data });
-          // myMap.map.addLayer({
-          //   'id': 'trace',
-          //   'type': 'line',
-          //   'source': 'trace',
-          //   'paint': {
-          //     'line-color': 'yellow',
-          //     'line-opacity': 0.75,
-          //     'line-width': 5
-          //   }
-          // });
 
           let points = data.length
           console.log("length is: " + points)
 
           // setup the viewport
-          myMap.map.easeTo({ 'center': data[data.length - 1].geometry.coordinates, 'zoom': zoom, 'speed' : '1' });
+          myMap.map.jumpTo({ 'center': data[data.length - 1].geometry.coordinates, 'speed' : '1' });
           myMap.map.setPitch(50);
 
           console.log( "centering to", data[data.length - 1].geometry.coordinates);
@@ -74,12 +71,7 @@ function initiateHurricane () {
 
               // myMap.map.getSource('trace').setData(data);
               myMap.map.setBearing(bearingBetween(data[data.length - 2].geometry.coordinates, data[data.length - 1].geometry.coordinates));
-              myMap.map.flyTo({ 'center' : data[data.length - 1].geometry.coordinates, 'speed' : '1', 'curve' : '1',' essential' : 'true', 'animate' : 'false'});
-              
-              // const latitude = coordinates[i][0]
-              // const longitude = coordinates[i][1]
-              // append(geoJSONlatlong, coordinates[i])
-              // print(geoJSONlatlong.length)
+              myMap.map.jumpTo({ 'center' : data[data.length - 1].geometry.coordinates, 'speed' : '1', 'curve' : '1',' essential' : 'true', 'animate' : 'false'});
 
               // it is necessary to re-calculate the latLngToPixel every step!
               clear()
@@ -90,27 +82,6 @@ function initiateHurricane () {
 
   });
 }
-
-// function drawGradient(x, y) {
-//   let radius = dim / 2;
-//   n = 7;
-//   for (let r = radius; r > 0; --r) { 
-//     if( r % n == 0) {
-//       // from radius downwards, draw a circle and increase hue each time
-//       // you want to decrease alpha instead :)
-//       noStroke()
-//       fill (130, 100, 43, 0.5)
-//       ellipse(x, y, 5, 5);
-//     }
-//   }
-// }
-
-// function drawCircle() {
-//     noFill() 
-//     ellipse(windowWidth / 4, windowHeight / 2, windowHeight / 6, windowHeight / 6)
-//     stroke(120, 90, 90, 1)
-//     strokeWeight(2)  
-// }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
