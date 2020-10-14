7ee08d61e039df778a5010532254f600be446fdb
rokeby <gary.zhexi.zhang@gmail.com>
2020-10-14 02:13:06 +0100

    still trying to figure out the latlngpixel bug that causes a point to be far off the page

--- public/p5/fudmapvis.js
+++ public/p5/fudmapvis.js
@@ -5,16 +5,17 @@ let myMap;
 let canvas;
 let hurricanes;
 const geoJSONlatlong = []
-const zoom = 6
+const zoom = 8
 let preJSON
 let array = []
 let latlong = []
 let dim = 100
 let stage = 1
-
+let pitch = 50
 
 //things below this line we are using
 const interpNum = 50;
+let interpCount = 1
 
 let currentData = {}
 let geojsonPoint = {
@@ -67,7 +68,7 @@ function setup() {
     zoom: zoom,
     studio: true, // false to use non studio styles
     style: 'mapbox://styles/rokeby/ckfvgjjsy6vkw19mkij8g9v3c',
-    pitch: 50,
+    pitch: pitch,
     bearing: bearing,
   }
 
@@ -129,7 +130,6 @@ function drawCanvas(startPoint, endPoint) {
 
   geojsonPoint.features[0].geometry.coordinates = [];
 
-
   const line = turf.lineString([startPoint, endPoint]);
   const lineCollection = turf.featureCollection([line]);
   const lineLength = turf.lineDistance(line);
@@ -149,31 +149,31 @@ function drawCanvas(startPoint, endPoint) {
       // push these coordinates to the feature collection we created
       rectCollection.features.push(pointOnLine);
   }
+     
+      if (interpCount < rectCollection.features.length) {
 
-  for (var n = 2; n < rectCollection.features.length; n++) {
+          for(let j = 1; j < interpCount; j++) {
 
-    // var interpolationTimer = window.setInterval(function () {
-           
-      if (n < rectCollection.features.length) {
+          var lat = rectCollection.features[j].geometry.coordinates[0]
+          var long = rectCollection.features[j].geometry.coordinates[1]
+          var prevlat = rectCollection.features[j-1].geometry.coordinates[0]
+          var prevlong = rectCollection.features[j-1].geometry.coordinates[1]
 
-            var lat = rectCollection.features[n].geometry.coordinates[0]
-            var long = rectCollection.features[n].geometry.coordinates[1]
-            var prevlat = rectCollection.features[n-1].geometry.coordinates[0]
-            var prevlong = rectCollection.features[n-1].geometry.coordinates[1]
-    
-            const latlong = myMap.latLngToPixel(long, lat)
-            const prevlatlong = myMap.latLngToPixel(prevlong, prevlat)
-    
-            x1 = latlong.x
-            x2 = prevlatlong.x
-            y1 = latlong.y
-            y2 = prevlatlong.y
-    
-            // drawBlueLine(x1, y1, x2, y2);
-            drawDots(x1,y1)
+          const latlong = myMap.latLngToPixel(long, lat)
+          const prevlatlong = myMap.latLngToPixel(prevlong, prevlat)
 
-          }
-  }
+          x1 = latlong.x
+          x2 = prevlatlong.x
+          y1 = latlong.y
+          y2 = prevlatlong.y
+
+          drawLine(x1, y1, x2, y2);
+          // drawDots(x1,y1)
+        }
+      interpCount++
+    } else {
+      interpCount = 1
+    }
 }
 
 function drawMap() {
@@ -190,8 +190,8 @@ function drawMap() {
   }
 
   myMap.map.jumpTo({ 'center': latestCoordinate, 'speed' : '1' });
-  myMap.map.setPitch(50);
-  console.log( "centering on", latestCoordinate, "in an array of", coordinates.length, ", the last point was", previousCoordinate);
+  myMap.map.setPitch(pitch);
+  // console.log( "centering on", latestCoordinate, "in an array of", coordinates.length, ", the last point was", previousCoordinate);
 
   if(coordinates.length > 2) {
     myMap.map.setBearing(bearingBetween(previousCoordinate, latestCoordinate));
@@ -202,7 +202,49 @@ function drawMap() {
   } 
   myMap.map.jumpTo({ 'center' : latestCoordinate, 'speed' : '1', 'curve' : '1',' essential' : 'true'});
 
+  // console.log(coordinates)
+
+  // mapBounds = myMap.map.getBounds();
+  // mylnglat = [40.6, -61.1]
+
+  // console.log(mapBounds._ne.lng, mapBounds._ne.lat, inBounds(mapBounds, mylnglat));
+
+  // pointNE = [bounds._ne.lat, bounds._ne.lng]
+  // pointSW = [bounds._sw.lat, bounds._sw.lng]
+
+  // var line = turf.lineString([pointNE, pointSW])
+  // var bboxBound = turf.bbox(line)
+  // var polygonBound = turf.bboxPolygon(bboxBound)
+  // var polygonBoundPoints = turf.polygon([polygonBound.geometry.coordinates[0]])
+
+  // console.log(coordinates[coordinates.length-1])
+  // console.log(polygonBoundPoints)
+  // console.log(polygonBound)
+  // console.log(turf.booleanPointInPolygon(turf.point([40.6, -61.1]), polygonBoundPoints))
+  // console.log(turf.booleanPointInPolygon(turf.point(coordinates[0]), polygonBoundPoints))
+  // console.log(polygonBound.geometry.coordinates[0])
+
+  // for (let k = 1; k < polygonBound.geometry.coordinates[0].length - 1; k ++) {
+  //     const pointLat = polygonBound.geometry.coordinates[0][k][0]
+  //     const pointLong = polygonBound.geometry.coordinates[0][k][1]
+  //     const prevPointLat = polygonBound.geometry.coordinates[0][k-1][1]
+  //     const prevPointLong = polygonBound.geometry.coordinates[0][k-1][1]
+
+  //     const pointLatLong = myMap.latLngToPixel(pointLong, pointLat)
+  //     const prevPointLatLong = myMap.latLngToPixel(prevPointLong, prevPointLat)
+  //     drawLine(pointLatLong.x, pointLatLong.y, prevPointLatLong.x, prevPointLatLong.y)
+
+  //   }
+
+  let pointsToDraw = []
+
   for (let i = 1; i < (coordinates.length - 1); i += 1) {
+
+    // if (turf.booleanPointInPolygon(turf.point(coordinates[i]), polygonBound) === true ) {
+    //   pointsToDraw.push(coordinates[i])
+    //   console.log("it's true!")
+    //   // console.log(pointsToDraw)
+    // }
 
       const pointLat = coordinates[i][0]
       const pointLong = coordinates[i][1]
@@ -210,7 +252,15 @@ function drawMap() {
       const prevPointLong = coordinates[i-1][1]
       const pointLatLong = myMap.latLngToPixel(pointLong, pointLat)
       const prevPointLatLong = myMap.latLngToPixel(prevPointLong, prevPointLat)
-      drawLine(pointLatLong.x, pointLatLong.y, prevPointLatLong.x, prevPointLatLong.y)
+
+      if (pointLatLong.x < 2000 && pointLatLong.y > -2000 && prevPointLatLong.x < 2000 && prevPointLatLong.y > -2000) {
+            drawLine(pointLatLong.x, pointLatLong.y, prevPointLatLong.x, prevPointLatLong.y)
+      } else {
+        console.log( "this line is too long!" )
+      }
+
+      // console.log(pointLatLong.x, pointLatLong.y, prevPointLatLong.x, prevPointLatLong.y)
+      // console.log(pointLat, pointLong)
   }
 
   if(coordinates.length > 2) {
@@ -218,6 +268,20 @@ function drawMap() {
   }
 }
 
+
+// const inBounds = function (bounds, lnglat) {
+//     let lng;
+
+//     const multLng = (lnglat[0] - bounds['_ne']['lng']) * (lnglat[0] - bounds['_sw']['lng']);
+//     if (bounds['_ne']['lng'] > bounds['_sw']['lng']) {
+//         lng = multLng < 0;
+//     } else {
+//         lng = multLng > 0;
+//     }
+
+//     const lat = (lnglat[1] - bounds['_ne']['lat']) * (lnglat[1] - bounds['_sw']['lat']) < 0;
+//     return lng && lat;
+// };
 
 function newHurricane() {
   console.log('time for a new hurricane')
@@ -245,7 +309,7 @@ async function listenForNewPoints() {
 
 window.setInterval( function() {
   listenForNewPoints()     
-}, 1000)
+}, 500)
   
 
 
