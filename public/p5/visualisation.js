21fc33d49d7aeb80762a166ea07547ac4ca1b930
Zhexi Zhang <zhexi-mbp-22@MacBook-Pro-3.local>
2025-04-24 19:39:22 +0800

    dotenv

--- public/p5/visualisation.js
+++ public/p5/visualisation.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const key = process.env.MAPKEY;
-const key = 'pk.eyJ1Ijoicm9rZWJ5IiwiYSI6ImNrZWViMjYzdDBqcjUzMm1sZ2IzdmxvdXMifQ.ZQFCehPe0Z7IRpR3k6vlKQ';
 const mappa = new Mappa('MapboxGL', key);
 let myMap;
 let canvas;
